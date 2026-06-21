74b21c832299ea40d5f84dbb5be67e6001b454fd
HassanHeravi <hasan.her85@gmail.com>
2026-06-22 01:15:32 +0330

    merge branch

--- src/pages/components/landing/HeroSection.jsx
+++ src/pages/components/landing/HeroSection.jsx
@@ -1,18 +1,18 @@
-import { Button } from "@heroui/button";
+import { Button, Chip  } from '@heroui/react';
 
 const Hero = () => {
   return (
-    <section className="my-10 mx-auto w-[90%] md:w-3/5 text-center" >
-      <div className="items-center justify-center flex flex-wrap gap-5 my-5 text-[33px] md:text-[48px]">
+    <section className="my-10 mx-auto w-[90%] md:w-3/5 text-center " >
+      <div className="items-center justify-center flex flex-wrap gap-5 my-5 text-[33px] text md:text-[48px]">
         <p className=" ">تجربه‌ای بی‌نظیر در یادگیری</p>
-        <Button 
+        <Chip 
                   isIconOnly 
                   disableRipple
                   variant="flat" 
-                  className="hidden md:block text-xl w-12 h-12 rounded-full text-center shadow-xl"
+                  className="hidden md:block text-xl w-12 h-12 rounded-full flex justify-center items-center text-center shadow-xl"
                 >
         <img src="/public//icons/code-circle 1.png" alt="" className="text-4xl mx-auto"/>
-        </Button>
+        </Chip>
         <p className="">کدنویسی؛</p>
       </div>
       <p className="text-[33px] md:text-[48px] my-5">از <span className="text-blue-500" >مبتدی</span> تا <span className="text-red-500">حرفه ای</span> !</p>
--- src/pages/components/landing/NavbarHeader.jsx
+++ src/pages/components/landing/NavbarHeader.jsx
@@ -1,7 +1,7 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
 import { useTheme } from "@heroui/use-theme";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { Moon02Icon, Sun03Icon } from "@hugeicons/core-free-icons";
+import { Moon02Icon, Sun03Icon,Menu02Icon } from "@hugeicons/core-free-icons";
 import { Button } from "@heroui/button";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -24,10 +24,14 @@ export default function NavbarHeader() {
     <Navbar maxWidth="full"  className=" relative mx-auto w-[95%] flex items-center justify-between p-1.5 font-extralight" >
       <NavbarContent >
         <NavbarBrand className="gap-3">
-          <div className="md:w-[34px] md:h-[33px] w-10 h-10 bg-primary rounded-xl flex items-center justify-center text-white font-black shadow-lg shadow-primary/30">
-            <img src="/icons/Untitled-1 4.svg" alt="" style={{width:"100%", height:"100%"}}/>
-          </div>
-          <img src="/public/icons/Untitled-1 3.png" alt="" className="mt-4 w-[189.1px] h-[37.69px]"/>
+          <div className=" border w-full overflow-hidden  h-11.75 flex items-center gap-1.5 px-1 ">
+          <img src="/public/images/imageLogo.png" alt="" className=" h-full w-0 opacity-0 lg:w-auto lg:opacity-100    duration-300 " />
+          <img
+            src="/public/images/textLogo.png"
+            alt=""
+            className=" max-h-[65%]  mt-0.75 w-38 "
+          />
+        </div>
         </NavbarBrand>
       </NavbarContent>
 
@@ -52,7 +56,7 @@ export default function NavbarHeader() {
           disableRipple
           variant="flat" 
           onPress={() => setTheme(theme === "light" ? "dark" : "light")}
-          className="text-xl border-solid border flex items-center justify-center dark:border-gray-600 rounded-full border-gray-200 text-center"
+          className="text-xl border-solid border flex items-center justify-center hover:bg-gray-100 dark:hover:bg-slate-800 dark:border-gray-600 duration-500 rounded-full border-gray-200 text-center"
         >
           {theme === "light" ? (
             <HugeiconsIcon icon={Moon02Icon} className=" m-0 w-4 h-4 " />
@@ -65,16 +69,17 @@ export default function NavbarHeader() {
           disableRipple
           color="primary" 
           variant="shadow" 
-          className="md:text-[16px] md:px-6 text-[16px] px-3 bg-blue-500 text-white rounded-3xl"
+          className=" bg-blue-500 text-white rounded-3xl flex items-center justify-center "
         >
-          <Link to={'/Auth/Login'} >ورود یا ثبت نام</Link>
+          <Link to={'/Auth/Login'} className=" text-sm mb-1 md:text-[16px] " >ورود یا ثبت نام</Link>
         </Button>
 
         <Button
-          disableRipple  className="block md:hidden w-[32px] h-[32px]"
+          variant="ghost"
+          disableRipple  className=" border-0 hover:bg-gray-100 dark:hover:bg-slate-800 rounded-lg duration-500 md:hidden text-xl w-9 h-9 flex items-center justify-center  text-center p-0 "
           onClick={() => {toggleMenu()}}
         >
-          <img src="icons/menu-02-stroke-rounded (1) 1.png" alt="" className="w-full h-full "/>
+          <HugeiconsIcon icon={Menu02Icon} className=" m-0 w-8 h-8   " />
         </Button>
       </NavbarContent>
     </Navbar>
